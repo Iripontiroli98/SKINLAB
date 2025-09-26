b7f836f9a9f49e393f0a0eb3bc192be6b5f7a8bc
Irina Pontiroli <iripontiroli@gmail.com>
2025-09-26 02:03:05 -0300

    Agrego seccion Como usar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
 import './style.css'
-
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburguesa = document.querySelector('.hamburguesa');
-    const navbar = document.querySelector('.navbar');
-    const header = document.querySelector('.hero'); // 👈 cambio aquí
-
-    hamburguesa.addEventListener('click', () => {
-        navbar.classList.toggle('mostrar');
-        header.classList.toggle('header-activo'); // le vas a dar estilos a esto en CSS
-    });
-});
