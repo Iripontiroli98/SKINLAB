964f92809d595982e2180a1d7eebb54675ddc2eb
Irina Pontiroli <iripontiroli@gmail.com>
2025-10-04 17:35:01 -0300

    Modifico seccion Historia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import './style.css'
+
+  document.addEventListener('DOMContentLoaded', () => {
+  const hamburguesa = document.querySelector('.hamburguesa');
+  const navbar = document.querySelector('.navbar');
+  const header = document.querySelector('.hero');
+
+  hamburguesa.addEventListener('click', () => {
+    navbar.classList.toggle('mostrar');
+    header.classList.toggle('header-activo'); // 👈 activa el fondo del navbar-top
+  });
+});
+
+
+const cards = document.querySelectorAll('.card');
+
+
+cards.forEach(card => {
+
+  card.addEventListener('mouseenter', () => {
+
+    card.classList.add('card-hovered');
+  });
+
+
+  card.addEventListener('mouseleave', () => {
+
+    card.classList.remove('card-hovered');
+  });
+});
